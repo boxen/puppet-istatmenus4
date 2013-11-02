08461a48a881a4bfdc85e64fdd5d29b629eea48f
Hiten Parmar <hiten@storenvy.com>
2013-11-02 04:42:16 -0700

    Upgrade to 4.10

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class istatmenus4 {
   package { 'iStatMenus4':
     provider => 'compressed_app',
-    source   => 'http://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus4.05.zip',
+    source   => 'http://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus4.10.zip',
   }
 }
